dcb03beb5a74e3bc11867e1b3ab6a2338f291f1c
UwU_Snek <thatsnekuwu@gmail.com>
2026-06-12 19:04:46 +0200

    Add default sprite for blocks with no item form

--- src/client/java/com/snek/engineersbliss/client/screens/rendering/BlockRenderer.java
+++ src/client/java/com/snek/engineersbliss/client/screens/rendering/BlockRenderer.java
@@ -1,7 +1,10 @@
 package com.snek.engineersbliss.client.screens.rendering;
 
+import com.snek.engineersbliss.EngineerSBliss;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
@@ -10,8 +13,10 @@ import net.minecraft.world.level.block.Block;
 
 
 public class BlockRenderer {
-    private BlockRenderer() {}
+    public static final Identifier MISSING_ITEM_SPRITE = Identifier.fromNamespaceAndPath(EngineerSBliss.MOD_ID, "textures/gui/missing_item_sprite.png");
+    public static final int DEFAULT_ITEM_SPRITE_SIZE = 16;
 
+    private BlockRenderer() {}
 
 
 
@@ -24,7 +29,7 @@ public class BlockRenderer {
      * @param y The Y position
      */
     public static void renderBlockIcon(GuiGraphicsExtractor graphics, Block block, int x, int y) {
-        renderBlockIcon(graphics, block, x, y, 16);
+        renderBlockIcon(graphics, block, x, y, DEFAULT_ITEM_SPRITE_SIZE);
     }
 
 
@@ -40,17 +45,20 @@ public class BlockRenderer {
      * @param size The size of the icon
      */
     public static void renderBlockIcon(GuiGraphicsExtractor graphics, Block block, int x, int y, int size) {
-        if(block.asItem() == Items.AIR) {
-            //TODO add "?" image for missing item forms
-        }
-        else {
-            float scale = size / 16f;
-            graphics.pose().pushMatrix();
-            graphics.pose().translate(x, y);
-            graphics.pose().scale(scale, scale);
-            graphics.item(new ItemStack(block), 0, 0);
-            graphics.pose().popMatrix();
-        }
+
+        // Set up pose
+        float scale = size / DEFAULT_ITEM_SPRITE_SIZE;
+        graphics.pose().pushMatrix();
+        graphics.pose().translate(x, y);
+        graphics.pose().scale(scale, scale);
+
+        // Load sprite
+        //! Blocks with no item form return AIR from .asItem()
+        if(block.asItem() == Items.AIR) graphics.blit(MISSING_ITEM_SPRITE, 0, 0, DEFAULT_ITEM_SPRITE_SIZE, DEFAULT_ITEM_SPRITE_SIZE, 0f, 1f, 0f, 1f);
+        else graphics.item(new ItemStack(block), 0, 0);
+
+        // Pop pose
+        graphics.pose().popMatrix();
     }
 
 
