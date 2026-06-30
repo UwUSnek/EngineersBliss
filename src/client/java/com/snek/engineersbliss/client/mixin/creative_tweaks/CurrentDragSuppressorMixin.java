d7ec14d807dad6f8fa03f886cad0d97b2a9e29fe
UwU_Snek <thatsnekuwu@gmail.com>
2026-06-30 04:25:48 +0200

    Add disable current drag feature

---
+++ src/client/java/com/snek/engineersbliss/client/mixin/creative_tweaks/CurrentDragSuppressorMixin.java
@@ -0,0 +1,29 @@
+package com.snek.engineersbliss.client.mixin.creative_tweaks;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.snek.engineersbliss.feature_handlers.creative_tweaks.CreativeTweakFeature;
+import com.snek.engineersbliss.feature_handlers.creative_tweaks.CreativeTweaksServerHandler;
+
+import net.minecraft.world.entity.player.Player;
+
+
+
+
+
+@Mixin(Player.class)
+public class CurrentDragSuppressorMixin {
+
+    @Inject(method = "isPushedByFluid", at = @At("RETURN"), cancellable = true, require = 1)
+    private void isPushedByFluid(final CallbackInfoReturnable<Boolean> cir) {
+        if(cir.getReturnValueZ()) {
+            final Player _this = (Player)(Object)this;
+            if(CreativeTweaksServerHandler.playerHasFeature(_this, CreativeTweakFeature.DISABLE_CURRENT_DRAG)) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
