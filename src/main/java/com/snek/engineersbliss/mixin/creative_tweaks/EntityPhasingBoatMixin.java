b93523e6b68e1d299deb801b5f0864d7084e005a
UwU_Snek <thatsnekuwu@gmail.com>
2026-06-30 04:38:57 +0200

    Fix entity phasing feature not working with vehicles

---
+++ src/main/java/com/snek/engineersbliss/mixin/creative_tweaks/EntityPhasingBoatMixin.java
@@ -0,0 +1,27 @@
+package com.snek.engineersbliss.mixin.creative_tweaks;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.snek.engineersbliss.feature_handlers.creative_tweaks.CreativeTweakFeature;
+import com.snek.engineersbliss.feature_handlers.creative_tweaks.CreativeTweaksServerHandler;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.boat.AbstractBoat;
+
+
+
+
+@Mixin(AbstractBoat.class)
+public class EntityPhasingBoatMixin {
+
+
+    @Inject(method = "push", at = @At("HEAD"), cancellable = true, require = 1)
+    public void push(final Entity entity, final CallbackInfo ci) {
+        if(CreativeTweaksServerHandler.playerHasFeature(entity, CreativeTweakFeature.PHASE_THROUGH_ENTITIES)) {
+            ci.cancel();
+        }
+    }
+}
