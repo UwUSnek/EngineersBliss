8a8db9e73fbbc39ba5a1483f9a381486940585ae
UwU_Snek <thatsnekuwu@gmail.com>
2026-07-09 17:39:21 +0200

    Implement disable item change animation feature

---
+++ src/client/java/com/snek/engineersbliss/client/mixin/creative_tweaks/HandAnimationsSuppressorMixin.java
@@ -0,0 +1,93 @@
+package com.snek.engineersbliss.client.mixin.creative_tweaks;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.snek.engineersbliss.client.feature_handlers.creative_tweaks.CreativeTweakFeature;
+import com.snek.engineersbliss.client.feature_handlers.creative_tweaks.CreativeTweaksHandler;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.world.entity.HumanoidArm;
+
+
+
+
+
+
+
+
+@SuppressWarnings("java:S4144")
+@Mixin(ItemInHandRenderer.class)
+public class HandAnimationsSuppressorMixin {
+
+
+    @SuppressWarnings("unused")
+    @Inject(method = "swingArm", at = @At("HEAD"), cancellable = true, require = 1)
+    private void engineersBliss$swingArm(final float attack, final PoseStack poseStack, final int invert, final HumanoidArm arm, final CallbackInfo ci) {
+        if(CreativeTweaksHandler.clientPlayerHasFeature(Minecraft.getInstance().player, CreativeTweakFeature.DISABLE_HAND_SWING_ANIMATION)) {
+            ci.cancel();
+        }
+    }
+
+
+
+
+
+
+
+    //! This blocks the main hand height recalculation cause by item changes
+    @SuppressWarnings("unused")
+    @ModifyExpressionValue(
+        method = "renderHandsWithItems",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/item/ItemModelResolver;swapAnimationScale(Lnet/minecraft/world/item/ItemStack;)F",
+            ordinal = 0
+        )
+    )
+    private float engineersBliss$swapAnimationScaleMainHand(float original) {
+        if(CreativeTweaksHandler.clientPlayerHasFeature(Minecraft.getInstance().player, CreativeTweakFeature.DISABLE_ITEM_CHANGE_ANIMATION)) {
+            return 0.0F;
+        }
+        return original;
+    }
+
+
+
+
+    //! This blocks the off hand height recalculation cause by item changes
+    @SuppressWarnings("unused")
+    @ModifyExpressionValue(
+        method = "renderHandsWithItems",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/item/ItemModelResolver;swapAnimationScale(Lnet/minecraft/world/item/ItemStack;)F",
+            ordinal = 1
+        )
+    )
+    private float engineersBliss$swapAnimationScaleOffHand(float original) {
+        if(CreativeTweaksHandler.clientPlayerHasFeature(Minecraft.getInstance().player, CreativeTweakFeature.DISABLE_ITEM_CHANGE_ANIMATION)) {
+            return 0.0F;
+        }
+        return original;
+    }
+
+
+
+
+    //! This forces the item to change instantly instead of waiting for an animation that doesn't exist anymore
+    @SuppressWarnings("unused")
+    @ModifyReturnValue(method = "shouldInstantlyReplaceVisibleItem", at = @At("RETURN"), require = 1)
+    private boolean engineersBliss$shouldInstantlyReplaceVisibleItem(boolean original) {
+        if(CreativeTweaksHandler.clientPlayerHasFeature(Minecraft.getInstance().player, CreativeTweakFeature.DISABLE_ITEM_CHANGE_ANIMATION)) {
+            return true;
+        }
+        return original;
+    }
+}
