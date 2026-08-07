fc493e8198804a1d422ddd1f1e27d6a60ec1e316
UwU_Snek <thatsnekuwu@gmail.com>
2026-08-07 13:21:53 +0200

    Update crosshair height fix mixin

--- src/client/java/com/snek/engineersbliss/client/mixin/status_bar/CrosshairHeightFixMixin.java
+++ src/client/java/com/snek/engineersbliss/client/mixin/status_bar/CrosshairHeightFixMixin.java
@@ -3,7 +3,9 @@ package com.snek.engineersbliss.client.mixin.status_bar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.snek.engineersbliss.client.feature_handlers.ClientFeatureSync;
 import com.snek.engineersbliss.client.screens.status_bar.StatusBarRenderer;
+import com.snek.engineersbliss.feature_handlers.settings.SettingsServerFeatureSet;
 
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -26,6 +28,13 @@ public class CrosshairHeightFixMixin {
         require = 2
     )
     private int eb$restoreCrosshairHeight(GuiGraphicsExtractor instance) {
-        return instance.guiHeight() + StatusBarRenderer.STATUS_BAR_HEIGHT;
+
+        //! Position=TOP doesn't require any fix.
+        if(!ClientFeatureSync.getFeatureB(SettingsServerFeatureSet.STATUS_BAR_POSITION)) {
+            return instance.guiHeight() + ClientFeatureSync.getFeatureI(SettingsServerFeatureSet.STATUS_BAR_HEIGHT);
+        }
+        else {
+            return instance.guiHeight();
+        }
     }
 }
