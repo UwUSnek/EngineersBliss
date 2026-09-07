0f37d714b800e96d08676d79a90f3100f34e93ea
UwU_Snek <thatsnekuwu@gmail.com>
2026-09-07 15:47:48 +0200

    Fix UiTxt reporting an incorrect width after GUI Scale changes

--- src/client/java/com/snek/engineersbliss/client/ui/renderer/UiGraphics.java
+++ src/client/java/com/snek/engineersbliss/client/ui/renderer/UiGraphics.java
@@ -135,8 +135,8 @@ public class UiGraphics {
 
         // Draw scaled text
         raw.pose().pushMatrix();
-        raw.pose().scale(textScale);
         raw.pose().translate(shiftX, shiftY);
+        raw.pose().scale(textScale);
         raw.text(scaledFont.getFont(), text, _x, _y, color);
         raw.pose().popMatrix();
     }
