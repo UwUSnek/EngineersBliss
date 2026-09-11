1dddd354ea73e072cbffbacf23585c49eff0052c
UwU_Snek <thatsnekuwu@gmail.com>
2026-09-11 16:14:29 +0200

    Remove Julia Set screen and renderer

--- src/client/java/com/snek/engineersbliss/client/screens/pause_screen/PauseScreenContent.java
+++ src/client/java/com/snek/engineersbliss/client/screens/pause_screen/PauseScreenContent.java
@@ -1,12 +1,7 @@
 package com.snek.engineersbliss.client.screens.pause_screen;
 
-import java.util.concurrent.TimeUnit;
-
-import org.jetbrains.annotations.Nullable;
-
 import com.snek.engineersbliss.EngineerSBliss;
 import com.snek.engineersbliss.client.EngineerSBlissClient;
-import com.snek.engineersbliss.client.feature_handlers.ClientFeatureSync;
 import com.snek.engineersbliss.client.screens.alt_textures.AltTexturesScreen;
 import com.snek.engineersbliss.client.screens.creative_tweaks.CreativeTweaksScreen;
 import com.snek.engineersbliss.client.screens.macros.MacrosScreen;
@@ -16,19 +11,12 @@ import com.snek.engineersbliss.client.screens.settings.SettingsScreen;
 import com.snek.engineersbliss.client.ui.base.__base_UiSidebarScreen;
 import com.snek.engineersbliss.client.ui.data_types.TextAlignment;
 import com.snek.engineersbliss.client.ui.font.Fonts;
-import com.snek.engineersbliss.client.ui.renderer.UiGraphics;
 import com.snek.engineersbliss.client.ui.widgets.buttons.UiPauseScreenButton;
-import com.snek.engineersbliss.client.ui.widgets.misc.PlayerMannequin;
 import com.snek.engineersbliss.client.ui.widgets.misc.PlayerModelWidget;
 import com.snek.engineersbliss.client.ui.widgets.misc.UiSpacer;
 import com.snek.engineersbliss.client.ui.widgets.misc.UiTextWidget;
 import com.snek.engineersbliss.client.utils.Layout;
-import com.snek.engineersbliss.client.utils.MinecraftUtils;
 import com.snek.engineersbliss.client.utils.UiTxt;
-import com.snek.engineersbliss.feature_handlers.settings.SettingsServerFeatureSet;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 
 
 
@@ -110,80 +98,10 @@ public class PauseScreenContent extends __base_UiSidebarScreen {
         leftSidebar.addWidgetAndSpacer(new UiPauseScreenButton(this, new UiTxt("Render stats"    ), "render_stats",     ()->{return null;}), Layout.BORDER_HEIGHT);
         leftSidebar.addWidgetAndSpacer(new UiPauseScreenButton(this, new UiTxt("World stats"     ), "world_stats",      ()->{return null;}), Layout.BORDER_HEIGHT);
         leftSidebar.addWidgetAndSpacer(new UiPauseScreenButton(this, new UiTxt("About"           ), "about",            ()->{return null;}), Layout.BORDER_HEIGHT);
-
-        // // Julia set
-        // final UiButton juliaScreenButton = new UiPauseScreenButton(screen, new UiTxt("??"), null, JuliaSetScreen::new, '\0');
-        // addRenderableWidget(juliaScreenButton);
-        // juliaScreenButton.setSize(Layout.BUTTON_HEIGHT, Layout.BUTTON_HEIGHT);
-        // juliaScreenButton.setX(width  - Layout.BUTTON_HEIGHT - Layout.BUTTON_HEIGHT);
-        // juliaScreenButton.setY(height - Layout.BUTTON_HEIGHT - Layout.BUTTON_HEIGHT);
     }
 
 
 
-
-
-
-
-
-    // @Override
-    // public void extractRenderState(final UiGraphics graphics, final float mouseX, final float mouseY, final float delta) {
-    //     super.extractRenderState(graphics, mouseX, mouseY, delta);
-
-
-    //     // Draw player model and name
-    //     final LocalPlayer player = Minecraft.getInstance().player;
-    //     if(player != null && ClientFeatureSync.getFeatureB(SettingsServerFeatureSet.PLAYER_MODEL_IN_PAUSE_SCREEN)) {
-
-    //         // Calculate dimensions and position
-    //         float modelScale = 64f;
-    //         float boxSize = Math.max(width, height);
-    //         final float xc = (vanillaClusterRight + width) / 2f;
-    //         final float x0 = xc                    - boxSize / 2f;
-    //         final float y0 = vanillaClusterCenterY - boxSize / 2f;
-    //         final float x1 = x0 + boxSize;
-    //         final float y1 = y0 + boxSize;
-    //         System.out.println("y center: " + vanillaClusterCenterY);
-    //         System.out.println("x0: " + x0 + ", x1: " + x1);
-
-    //         // Get mannequin
-    //         final @Nullable PlayerMannequin model = PlayerMannequin.getMannequin();
-    //         if(model != null) {
-    //             // graphics.enableScissor(0, 0, width, height);
-    //             graphics.entity(x0, y0, x1, y1, modelScale, 0.0f, mouseX, mouseY, model);
-    //             // graphics.disableScissor();
-    //         }
-
-    //         // Calculate play time
-    //         final long ms = MinecraftUtils.getPlaytimeMs();
-    //         final long hours   = TimeUnit.MILLISECONDS.toHours  (ms);
-    //         final long minutes = TimeUnit.MILLISECONDS.toMinutes(ms) % 60;
-    //         final long seconds = TimeUnit.MILLISECONDS.toSeconds(ms) % 60;
-
-    //         // Calculate text dimensions and position
-    //         final UiTxt playerName = new UiTxt(String.format("%s", player.getGameProfile().name()),             Fonts.ui.regular, Layout.HEADER_SCALE);
-    //         final UiTxt playTime   = new UiTxt(String.format("Playtime: %dh %dm %ds", hours, minutes, seconds), Fonts.ui.light);
-    //         int nameY = Math.round(vanillaClusterCenterY - modelScale - playerName.getScaledFont().getLineHeight() - playTime.getScaledFont().getLineHeight());
-    //         int titleY = nameY + playerName.getScaledFont().getLineHeight() + 2;
-
-    //         // Draw player name an title
-    //         graphics.text(playerName, Math.round(xc),  nameY, 0xFFFFC200, TextAlignment.CENTER_ANCHORED, 0, true);
-    //         graphics.text(playTime,   Math.round(xc), titleY, 0xFFDDDDDD, TextAlignment.CENTER_ANCHORED, 0, true);
-    //     }
-    // }
-
-    // @Override
-    // public void extractRenderState(final UiGraphics graphics, final float mouseX, final float mouseY, final float delta) {
-    //     super.extractRenderState(graphics, mouseX, mouseY, delta);
-
-    //     // final float boxSize = Math.max(width, height); //TODO ??
-    //     final float boxSize = 500;
-    //     final float xc = (vanillaClusterRight + width) / 2f;
-    //     playerModel.setPos(xc - boxSize / 2f, vanillaClusterCenterY - boxSize / 2f);
-    //     playerModel.setSize(boxSize, boxSize);
-    //     playerModel.relayout();
-    //     playerModel.extractWidgetRenderState(graphics, mouseX, mouseY, delta);
-    // }
 
     @Override
     public void relayoutSelf() {
