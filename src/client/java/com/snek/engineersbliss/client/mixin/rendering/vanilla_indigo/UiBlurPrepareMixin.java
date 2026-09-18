5fa1b338065fb53daf31131ac6c3eebee20fcdde
UwU_Snek <thatsnekuwu@gmail.com>
2026-09-19 01:25:26 +0200

    Code cleanup

--- src/client/java/com/snek/engineersbliss/client/mixin/rendering/vanilla_indigo/UiBlurPrepareMixin.java
+++ src/client/java/com/snek/engineersbliss/client/mixin/rendering/vanilla_indigo/UiBlurPrepareMixin.java
@@ -1,36 +1,17 @@
 package com.snek.engineersbliss.client.mixin.rendering.vanilla_indigo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.snek.engineersbliss.client.ui.renderer.GaussianBlurPipRenderer;
 import com.snek.engineersbliss.client.ui.renderer.UiBlur;
 
 import net.minecraft.client.gui.render.GuiRenderer;
-import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
 import net.minecraft.client.renderer.GameRenderer;
 
 
 
 
-// @Mixin(GuiRenderer.class)
-// public abstract class GaussianBlurRenderPassMixin {
-
-//     @SuppressWarnings("unused")
-//     @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
-//     private List<PictureInPictureRenderer<?>> eb$addGaussianBlurRenderer(final List<PictureInPictureRenderer<?>> renderers) {
-//         final List<PictureInPictureRenderer<?>> combined = new ArrayList<>(renderers);
-//         combined.add(new GaussianBlurPipRenderer());
-//         return combined;
-//     }
-// }
 @Mixin(GuiRenderer.class)
 public abstract class UiBlurPrepareMixin {
 
